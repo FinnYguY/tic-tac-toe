819ac5d17f6bed86048b97ad11f1b70889bc7ce2
FinnYguY <tema.menschikoff2013165@gmail.com>
2023-09-12 16:25:02 +0300

    feat: game can now determine the winner

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -3,49 +3,68 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 import Cell from "../Cell";
+import { calculateWinner } from "../../constants/functions";
 
 export default function Board() {
-  const xmark = (
-    <FontAwesomeIcon
-      className="tic-tac-toe xmark"
-      icon={icon({ name: "xmark" })}
-    />
-  );
-  const checkmark = (
-    <FontAwesomeIcon
-      className="tic-tac-toe checkmark"
-      icon={icon({ name: "check", style: "solid" })}
-    />
-  );
+  const xmark = {
+    name: "xmark",
+    icon: (
+      <FontAwesomeIcon
+        className="tic-tac-toe xmark"
+        icon={icon({ name: "xmark" })}
+      />
+    ),
+  };
+  const checkmark = {
+    name: "checkmark",
+    icon: (
+      <FontAwesomeIcon
+        className="tic-tac-toe checkmark"
+        icon={icon({ name: "check", style: "solid" })}
+      />
+    ),
+  };
 
-  const [cells, setCells] = useState(Array(9).fill(null));
+  const [cells, setCells] = useState(Array(9).fill({ name: "", icon: "" }));
   const [xIsNext, setXIsNext] = useState(true);
+  // const [status, setStatus] = useState("");
 
   function handleClick(i) {
-    if (cells[i]) return;
+    if (calculateWinner(cells) || cells[i].icon) {
+      return;
+    }
     const nextCells = cells.slice();
     xIsNext ? (nextCells[i] = xmark) : (nextCells[i] = checkmark);
     setCells(nextCells);
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(cells);
+  let status = "";
+  winner
+    ? (status = `Winner: ${winner}`)
+    : (status = `Current move: ${xIsNext ? xmark.name : checkmark.name}`);
+
   return (
-    <div className="board">
-      <div className="board__row">
-        <Cell value={cells[0]} onCellClick={() => handleClick(0)} />
-        <Cell value={cells[1]} onCellClick={() => handleClick(1)} />
-        <Cell value={cells[2]} onCellClick={() => handleClick(2)} />
+    <>
+      <div style={{ color: "white" }}>{status}</div>
+      <div className="board">
+        <div className="board__row">
+          <Cell value={cells[0]} onCellClick={() => handleClick(0)} />
+          <Cell value={cells[1]} onCellClick={() => handleClick(1)} />
+          <Cell value={cells[2]} onCellClick={() => handleClick(2)} />
+        </div>
+        <div className="board__row">
+          <Cell value={cells[3]} onCellClick={() => handleClick(3)} />
+          <Cell value={cells[4]} onCellClick={() => handleClick(4)} />
+          <Cell value={cells[5]} onCellClick={() => handleClick(5)} />
+        </div>
+        <div className="board__row">
+          <Cell value={cells[6]} onCellClick={() => handleClick(6)} />
+          <Cell value={cells[7]} onCellClick={() => handleClick(7)} />
+          <Cell value={cells[8]} onCellClick={() => handleClick(8)} />
+        </div>
       </div>
-      <div className="board__row">
-        <Cell value={cells[3]} onCellClick={() => handleClick(3)} />
-        <Cell value={cells[4]} onCellClick={() => handleClick(4)} />
-        <Cell value={cells[5]} onCellClick={() => handleClick(5)} />
-      </div>
-      <div className="board__row">
-        <Cell value={cells[6]} onCellClick={() => handleClick(6)} />
-        <Cell value={cells[7]} onCellClick={() => handleClick(7)} />
-        <Cell value={cells[8]} onCellClick={() => handleClick(8)} />
-      </div>
-    </div>
+    </>
   );
 }
--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Cell({ value, onCellClick }) {
   return (
     <button onClick={onCellClick} className="board__cell">
-      {value}
+      {value?.icon}
     </button>
   );
 }
