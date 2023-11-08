3fd9e778b9b6a52223f1b2d56aa38dbe81cc9990
FinnYguY <tema.menschikoff2013165@gmail.com>
2023-11-08 15:35:47 +0300

    feat: added loader and changed how background is treated

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,15 +1,30 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import classNames from "classnames";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { selectCurrentTheme } from "../../store/themeSlice";
+import Loader from "../Loader";
 
 export default function Background() {
   const currentTheme = useSelector(selectCurrentTheme);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [bgImage, setBgImage] = useState("");
 
-  let backgroundClassName = classNames([
-    "background",
-    `background--${currentTheme}`,
-  ]);
+  useEffect(() => {
+    let preloadImg = document.createElement("img");
+    let imgUrl = `${process.env.PUBLIC_URL}/backgrounds/bg-${currentTheme}.jpg`;
+    preloadImg.src = imgUrl;
+    preloadImg.onload = () => {
+      setBgImage(imgUrl);
+      setIsLoaded(true);
+    };
+  }, [currentTheme]);
 
-  return <div className={backgroundClassName}></div>;
+  return (
+    <>
+      <Loader isVisible={!isLoaded} />
+      <div
+        className="background"
+        style={{ background: `center / cover no-repeat url(${bgImage})` }}
+      ></div>
+    </>
+  );
 }
